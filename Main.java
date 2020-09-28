427292ebfd9e100552cac34774ba54b140a3d629
ViralVirus123 <alexander.hammond-garcia@patelhs.org>
2020-09-28 02:43:02 +0000

    Added variables and if/else statements

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
+
+  class Main
+ {
+  public static void main(String[] args) 
+   {
+    int myAge = 15;
+    int myBirthday = 06/11;
+    int todaysDate = 9/21;
+    
+    if (myBirthday != todaysDate) {
+      System.out.println("I am " + myAge + " years old.");
+    }else{
+      System.out.println("I am still " + myAge + " years old.");
+      }
+   }
+ }
